2d31a7c97b0871ea6a998d9b230f6f647a1a15cd
dengxianxiaoo <dengxianxiao@gmail.com>
2017-11-14 01:23:40 +0800

    webpack hot middleware

--- interesting/webpack/webpack-demo/webpack.config.js
+++ interesting/webpack/webpack-demo/webpack.config.js
@@ -1,9 +1,36 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output management'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+     rules: [
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          }
+     ]
+   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   }
 };
